d702513fa829e0593754b31f6429c31a1044b641
saqmu <samuelcostaortiz2025@gmail.com>
2026-06-24 16:50:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Seleciona o bloco de notas
 const bloco = document.getElementById('bloco-notas');
 
-// Quando o site carregar, ele verifica se tem algo salvo na memória
+// Quando o site carregar, ele verifica se tem algo salvo na memória do navegador
 window.onload = function() {
     const textoSalvo = localStorage.getItem('nota_usuario');
     if (textoSalvo) {
@@ -9,7 +9,7 @@ window.onload = function() {
     }
 };
 
-// Toda vez que você digitar uma letra, ele salva na memória do navegador
+// Toda vez que você digitar qualquer letra, ele salva instantaneamente
 bloco.addEventListener('input', () => {
     localStorage.setItem('nota_usuario', bloco.value);
 });
