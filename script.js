60fe90dca2600540546bea56b54ef6c9497795f0
saqmu <samuelcostaortiz2025@gmail.com>
2026-06-24 17:01:20 -0400

    Fix syntax errors in script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Seleciona o bloco de notas
 const bloco = document.getElementById('bloco-notas');
 
-// Quando o site carregar, ele verifica se tem algo salvo na memória do navegador
 window.onload = function() {
     const textoSalvo = localStorage.getItem('nota_usuario');
     if (textoSalvo) {
@@ -9,7 +7,6 @@ window.onload = function() {
     }
 };
 
-// Toda vez que você digitar qualquer letra, ele salva instantaneamente
 bloco.addEventListener('input', () => {
     localStorage.setItem('nota_usuario', bloco.value);
 });
